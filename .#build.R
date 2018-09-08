e403b9973328ab857fb1884b47229c2f8fbe463b
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2018-09-07 21:53:47 -0400

    :construction: split out RandMat options into functions. Started slow and tested first to make sure it works. #50

--- .#build.R
+++ .#build.R
@@ -1,7 +1,8 @@
 #!/usr/local/bin/Rscript
 require(devtools)
 devtools::document('R')
-devtools::check(".")
+devtools::check(".", args = c("--no-examples", "--no-tests"))
+#devtools::check(".")
 devtools::install(".")
 
 
